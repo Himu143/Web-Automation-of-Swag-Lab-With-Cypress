8140a23d631459b057484073f6c2a95a944cacb2
tajulislam <tajulislam@inneed.cloud>
2025-02-26 15:20:03 +0600

    AddProduct valid add

--- cypress/pages/AddToCart.js
+++ cypress/pages/AddToCart.js
@@ -1,39 +1,34 @@
 class AddToCart {
+  userInDashBoard() {
+    cy.get(".title").should("have.text", "Products");
+  }
 
-    userInDashBoard(){
-        cy.get('.title').should("have.text","Products")
-    }
+  viewProduct() {
+    cy.get("#item_4_title_link > .inventory_item_name").click();
+  }
 
-    viewProduct() {
-
-        cy.get('#item_4_title_link > .inventory_item_name').click();
-    }
-
-    addingToCart(){
-
-        //cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-        cy.get('[data-test="add-to-cart"]').click();
-    }
-    cartPage(){
-        cy.screenshot().get('.shopping_cart_link').click();
-    }
-    checkOutPage()
-    {
-        cy.get('[data-test="checkout"]').click();
-    }
-    userInformation(firstName,lastName,zip){
-        cy.get('[data-test="firstName"]').type(firstName);
-        cy.get('[data-test="lastName"]').type(lastName);
-        cy.get('[data-test="postalCode"]').type(zip);
-        cy.get('[data-test="continue"]').click();
-    }
-    completePurchase(){
-        cy.get('[data-test="finish"]').click();
-        cy.get('.title').should("have.text","Checkout: Complete!")
-    }
-    cancelPurchase(){
-        cy.get('[data-test="cancel"]').click();
-    }
-
+  addingToCart() {
+    //cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart"]').click();
+  }
+  cartPage() {
+    cy.screenshot().get(".shopping_cart_link").click();
+  }
+  checkOutPage() {
+    cy.get('[data-test="checkout"]').click();
+  }
+  userInformation(firstName, lastName, zip) {
+    cy.get('[data-test="firstName"]').type(firstName);
+    cy.get('[data-test="lastName"]').type(lastName);
+    cy.get('[data-test="postalCode"]').type(zip);
+    cy.get('[data-test="continue"]').click();
+  }
+  completePurchase() {
+    cy.get('[data-test="finish"]').click();
+    cy.get(".title").should("have.text", "Checkout: Complete!");
+  }
+  cancelPurchase() {
+    cy.get('[data-test="cancel"]').click();
+  }
 }
 export default AddToCart;

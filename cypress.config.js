3bb2a7578db656c59e899ae20faa73e8af1f7abd
tajulislam <tajulislam@inneed.cloud>
2025-02-26 18:19:49 +0600

    update ss, video

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  reporter: "cypress-mochawesome-reporter",
+  reporter:'cypress-mochawesome-reporter',
   e2e: {
+    video: false,  // Ensures Cypress records videos
+    screenshotOnRunFailure: false,
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      config.specPattern = ["cypress/e2e/LoginTest.cy.js"];
+      require('cypress-mochawesome-reporter/plugin')(on);
+      config.specPattern = [
+        'cypress/e2e/LoginTest.cy.js'
+      ]
       return config;
     },
   },
